39ae5be95207e80ba4c24f164906b2cc7c7d4e5b
yashodhicy <yashodhichathurangi94@gmail.com>
2023-01-04 20:02:34 +0200

    first commit

---
+++ print and fornat.py
@@ -0,0 +1,45 @@
+#from termcolor import colored
+
+print('Hello world!','we are coders.',sep=' <',end='/>\n')
+
+# print('Yes we can learn python',end=" and ")
+
+# print('we are capable of anything.')
+
+
+
+#input function
+# name = input('Enter your name:')
+
+# print('Hello '+ name  +', when is your birthday?')
+
+
+
+
+
+#format-function
+# year = input('Year:')
+# month=input('Month:')
+# day=input('Date:')
+
+#print(year,month,day,sep='/')
+
+
+#str.format(value1,value2,....)
+# bday="{}/{}/{}".format(day,month,year)
+# print("Hey guys,{}'s birthday is on'{}.let's celebrate.".format(name,bday))
+
+#f string
+# f"welcome {name}"
+# sentence=f"Hey guys,{name}'s birthday is on {bday}.Let's celebrate."
+# print(sentence)
+
+#print colored and bold letters
+
+# print(colored('Happy coding', 'red', attrs=['bold','underline']))
+
+#summery
+# print()
+# str.format()
+# f""
+# colored bold text
